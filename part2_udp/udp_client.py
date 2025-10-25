d5d3dd8e937fd8d0c790d6774c96e730b636a286
ayaanpuri30 <aypuri@ucdavis.edu>
2025-10-25 15:36:28 -0700

    part2 server side

---
+++ part2_udp/udp_client.py
@@ -0,0 +1,5 @@
+import socket
+
+SERVER_HOST = "127.0.0.1"
+SERVER_PORT = 9000
+SENTINEL = b"__END__"  
---
+++ part2_udp/udp_server.py
@@ -0,0 +1,64 @@
+import socket
+import time
+
+# server config
+HOST = "127.0.0.1"
+PORT = 9000
+BUFFER_SIZE = 65535  # max UDP payload size
+SENTINEL = b"__END__"  # final message from client
+TIMEOUT = 10.0 # in seconds
+
+# UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
+server_socket.settimeout(TIMEOUT)
+
+print(f"Server on {HOST}:{PORT}")
+
+total_bytes = 0
+start_time = None
+client_address = None
+
+while True:
+    try:
+        data, addr = server_socket.recvfrom(BUFFER_SIZE)
+    except socket.timeout:
+        print("No data received.")
+        break
+
+    client_address= addr
+
+    # first data packet -> start the timer
+    if start_time is None and data != SENTINEL:
+        start_time = time.perf_counter()
+
+    # last packet -> stop
+    if data == SENTINEL:
+        break
+
+    total_bytes += len(data)
+
+# calculate only if data
+if start_time is not None:
+    end_time = time.perf_counter()
+    duration = end_time - start_time
+    if duration <= 0:
+        duration = 1e-12
+
+    # convert to KBs (1024 bytes)
+    throughput = (total_bytes / 1024.0) / duration
+
+    # send to client
+    result_str = f"{throughput:.2f}"
+    server_socket.sendto(result_str.encode("ascii"), client_address)
+
+    # ssummary
+    print("\n[RESULT]")
+    print(f"Total bytes received: {total_bytes}")
+    print(f"Time taken: {duration:.6f} seconds")
+    print(f"Throughput: {result_str} KB/s")
+else:
+    print("[No data was received before timeout.")
+
+server_socket.close()
+print("Server closed.")
